aa6195db52d0c9180029e78fcea241406fb99606
aarora8 <aarora8@jhu.edu>
2018-05-27 02:51:38 -0400

    adding initial scoring scripts

--- egs/madcat_arabic/v1/local/scoring/scoring_utils.py
+++ egs/madcat_arabic/v1/local/scoring/scoring_utils.py
@@ -4,14 +4,13 @@ import numpy as np
 
 def _evaluate_data(ref_file, hyp_file):
 
-    det_matched = 0
+    hyp_matched = 0
     iou_mat = np.empty([1, 1])
     ref_pols = []
-    det_pols = []
+    hyp_pols = []
     ref_pol_points = []
-    det_pol_points = []
+    hyp_pol_points = []
     pairs = []
-    det_matched_nums = []
 
     pointlist = _get_pointlist(ref_file)
     for n in range(len(pointlist)):
@@ -23,39 +22,38 @@ def _evaluate_data(ref_file, hyp_file):
     pointlist = _get_pointlist(hyp_file)
     for n in range(len(pointlist)):
         points = pointlist[n]
-        det_polygon = _polygon_from_points(points)
-        det_pols.append(det_polygon)
-        det_pol_points.append(points)
+        hyp_polygon = _polygon_from_points(points)
+        hyp_pols.append(hyp_polygon)
+        hyp_pol_points.append(points)
 
-    if len(ref_pols) > 0 and len(det_pols) > 0:
-        output_shape = [len(ref_pols), len(det_pols)]
+    if len(ref_pols) > 0 and len(hyp_pols) > 0:
+        output_shape = [len(ref_pols), len(hyp_pols)]
         iou_mat = np.empty(output_shape)
         ref_rect_mat = np.zeros(len(ref_pols), np.int8)
-        det_rect_mat = np.zeros(len(det_pols), np.int8)
+        hyp_rect_mat = np.zeros(len(hyp_pols), np.int8)
         for ref_index in range(len(ref_pols)):
-            for hyp_index in range(len(det_pols)):
+            for hyp_index in range(len(hyp_pols)):
                 polygon_ref = ref_pols[ref_index]
-                polygon_hyp = det_pols[hyp_index]
+                polygon_hyp = hyp_pols[hyp_index]
                 iou_mat[ref_index, hyp_index] = _get_intersection_over_union(polygon_hyp, polygon_ref)
 
     for ref_index in range(len(ref_pols)):
-        for hyp_index in range(len(det_pols)):
-            if ref_rect_mat[ref_index] == 0 and det_rect_mat[hyp_index] == 0:
+        for hyp_index in range(len(hyp_pols)):
+            if ref_rect_mat[ref_index] == 0 and hyp_rect_mat[hyp_index] == 0:
                 if iou_mat[ref_index, hyp_index] > 0.5:
                     ref_rect_mat[ref_index] = 1
-                    det_rect_mat[hyp_index] = 1
-                    det_matched += 1
+                    hyp_rect_mat[hyp_index] = 1
+                    hyp_matched += 1
                     pairs.append({'reference_data': ref_index, 'det': hyp_index})
-                    det_matched_nums.append(hyp_index)
 
     num_ref = len(ref_pols)
-    num_hyp = len(det_pols)
+    num_hyp = len(hyp_pols)
     if num_ref == 0:
         recall = float(1)
         precision = float(0) if num_hyp > 0 else float(1)
     else:
-        recall = float(det_matched) / num_ref
-        precision = 0 if num_hyp == 0 else float(det_matched) / num_hyp
+        recall = float(hyp_matched) / num_ref
+        precision = 0 if num_hyp == 0 else float(hyp_matched) / num_hyp
     h_mean = 0 if (precision + recall) == 0 else 2.0 * precision * recall / (precision + recall)
 
     per_sample_metrics = dict()
@@ -64,9 +62,9 @@ def _evaluate_data(ref_file, hyp_file):
         'recall': recall,
         'h_mean': h_mean,
         'pairs': pairs,
-        'iou_mat': [] if len(det_pols) > 100 else iou_mat.tolist(),
-        'gt_pol_points': ref_pol_points,
-        'det_pol_points': det_pol_points
+        'iou_mat': [] if len(hyp_pols) > 100 else iou_mat.tolist(),
+        'ref_pol_points': ref_pol_points,
+        'hyp_pol_points': hyp_pol_points
     }
 
 
